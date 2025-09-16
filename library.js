bf16a27b75dd9ec6f1bf656bf0c32aa067cc9033
pratama404 <agengputrapratama@gmail.com>
2025-09-16 14:42:23 +0700

    ass func

--- library.js
+++ library.js
@@ -2,3 +2,8 @@ function
     libraryFunction() {
     console.log("This is a function from the library.");
 }
+
+function
+    anotherLibraryFunction() {
+    console.log("This is another function from the library.");
+}
